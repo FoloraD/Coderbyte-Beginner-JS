964d03c6ef233044a4f5a6e921e95d2206d223e6
FoloraD <fduang@gmail.com>
2021-09-24 17:20:23 +0100

    Created the function to verify valid username

--- Codeland Username Validation.js
+++ Codeland Username Validation.js
@@ -18,3 +18,30 @@ Input: "u__hello_world123"
 Output: true
 
 */
+
+// My solution
+
+
+
+function CodelandUsernameValidation(str) { 
+
+    // code goes here  
+
+    const validLength = (str) => str.length >= 4 && str.length <= 25;   // check username is between 4 and 25 characters.
+    const validAlphanumeric = /^[A-Za-z]\w+[A-Za-z0-9]$/;              
+                                                                    //  - `^`        Start from the beginning of the string.
+                                                                    //  - `[]`       Allow any character specified, including...
+                                                                    //  - `a-z`      anything from a to z,
+                                                                    //  - `A-Z`      anything from A to Z,
+                                                                    //  - `0-9`      anything from 0 to 9,
+                                                                    //   - `+`        one or more times (change to * to allow empty string)
+                                                                    //  - `\w`       and any alphanumeric character from the basic Latin alphabet, including the underscore.
+                                                                    //   - `$`       End of string 
+                                                                        
+
+    return validAlphanumeric.test(str) && validLength(str); 
+  
+  }
+     
+  // keep this function call here 
+  console.log(CodelandUsernameValidation(readline()));
